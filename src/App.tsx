024099dc8af12ebab7dae07bdf4e4b0891530ca4
Douglas Soares <douglasmaismcpe@gmail.com>
2022-04-25 20:00:17 -0300

    Added styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: #8257e6;
+  font-size: 50px;
+`;
 
 export function App() {
   return (
     <div className="App">
-      <h1>opa</h1>
+      <Title>opa</Title>
     </div>
   );
 }
