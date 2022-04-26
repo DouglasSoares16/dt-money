03f5fb0de9ba3458561fa72ba71661d5c8862b76
Douglas Soares <douglasmaismcpe@gmail.com>
2022-04-26 19:29:48 -0300

    Created the Setting of MirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { createServer } from "miragejs";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction Fake",
+          amount: 500,
+          type: "deposit",
+          categoy: "Food",
+          created_at: new Date()
+        }
+      ];
+    })
+  }
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
